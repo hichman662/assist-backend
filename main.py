d6b9a825bfa3e76facd70d3d4e52cd5855d5dd1e
Shahab Nasabeh <hichman662@gmail.com>
2025-02-20 11:57:38 +0100

    newYolov11

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+np.int = int
 import multiprocessing
 multiprocessing.set_start_method("spawn", force=True)
 import sys
--- routes/textReader_routes.py
+++ routes/textReader_routes.py
@@ -8,17 +8,21 @@ text_reader_ns = Namespace("text_reader", description="API for Text Reader")
 @text_reader_ns.route("/")
 class TextReader(Resource):
     def post(self):
-        """Handle image upload and send it to the OCR processing service."""
+        """
+        Handle image upload and send it to the OCR processing service.
+        """
         try:
             # Retrieve the image file from the request
             image_file = request.files.get("image")
             if not image_file:
-                return {"error": "No image file provided"}, 400
+                return {"status": "error", "message": "No image file provided"}, 400
+
+            print(f"Received file: {image_file.filename}, Content-Type: {image_file.content_type}")
 
             # Call the service and unpack the response
             response, status_code = process_text_image(image_file)
             return response, status_code
 
         except Exception as e:
-            print(f"Error occurred: {e}")
-            return {"error": "An unexpected error occurred"}, 500
+            print(f"Error occurred in text reader API: {e}")
+            return {"status": "error", "message": "An unexpected error occurred"}, 500
